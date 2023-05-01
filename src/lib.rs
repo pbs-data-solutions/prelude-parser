2fc811a2b2cea50401b8c5076e9055988ff31446
Paul Sanders <psanders1@gmail.com>
2023-05-01 07:45:20 -0400

    Parse to python class

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,36 @@ use pyo3::create_exception;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
+use quick_xml::reader::Reader;
 
 create_exception!(_prelude_parser, FileNotFoundError, PyException);
 create_exception!(_prelude_parser, InvalidFileType, PyException);
+create_exception!(_prelude_parser, ParsingError, PyException);
 
 fn parse_xml<'py>(py: Python<'py>, xml_file: &PathBuf) -> PyResult<&'py PyDict> {
-    let content = read_to_string(xml_file)?;
     let data = PyDict::new(py);
+    let reader = Reader::from_file(xml_file);
+    let mut buff = Vec::new();
+    let reader_data: Reader<BufReader<File>>;
+
+    match reader {
+        Ok(r) => let reader_data = r,
+        Err(e) => ParsingError:new_err(format!("An error occurred parsing the file: {:?}", e)).restore(py),
+    }
+
+    loop {
+        match reader.read_event_into(&mut buff) {
+            Err(e) => ParsingError:new_err(format!("An error occurred parsing the file: {:?}", e)).restore(py);
+        }
+    }
+    /*let content = read_to_string(xml_file)?;
+
+    for line in content.split('\n').skip(2) {
+        if line.len() > 0 {
+            println!("{line}");
+        }
+    }*/
+
     data.set_item("form_name", "demographics")?;
 
     Ok(data)
