239e5ad7c8798ed8fd3ee8e69b150748f94a4fd8
Paul Sanders <paul@paulsanders.dev>
2025-07-03 08:14:37 -0400

    Make version dynamic based on Cargo.toml version

--- prelude_parser/__init__.py
+++ prelude_parser/__init__.py
@@ -2,6 +2,7 @@ from prelude_parser._prelude_parser import (
     SiteNative,
     SubjectNative,
     UserNative,
+    __version__,
     parse_site_native_file,
     parse_site_native_string,
     parse_subject_native_file,
@@ -9,15 +10,13 @@ from prelude_parser._prelude_parser import (
     parse_user_native_file,
     parse_user_native_string,
 )
-from prelude_parser._version import VERSION
 from prelude_parser.parser import parse_to_classes, parse_to_dict
-
-__version__ = VERSION
 
 __all__ = [
     "SiteNative",
     "SubjectNative",
     "UserNative",
+    "__version__",
     "parse_site_native_file",
     "parse_site_native_string",
     "parse_subject_native_file",
